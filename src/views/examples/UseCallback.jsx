bbd415cac85d88e9e2ea2a9289097ab3e9c7605a
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-02-11 22:19:07 -0300

    useMemo e useCallBack

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+/* useCallback serve para economizar renderizações mantendo funções em cache */
+import React, { useState, useCallback } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import UseCallbackButtons from './UseCallBackButtons';
 
 const UseCallback = (props) => {
-    return (
-        <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
 
-export default UseCallback
+  const inc = useCallback(
+    function (delta) {
+      setCount((curr) => curr + delta);
+    },
+    [setCount]
+  );
+
+  return (
+    <div className="UseCallback">
+      <PageTitle
+        title="Hook UseCallback"
+        subtitle="Retorna uma função memoizada!"
+      />
+      <div className="center">
+        <span className="text">{count}</span>
+        <UseCallbackButtons inc={inc} />
+      </div>
+    </div>
+  );
+};
+
+export default UseCallback;
